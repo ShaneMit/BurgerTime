138bc1300dc3a8e90a862c9459ef07ef683de83a
ShaneMit <shanemit@cox.net>
2020-05-30 13:47:00 -0700

    created server

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-express = require('express')
+const express = require('express')
+const app = express()
+
+app.engine('.hbs', require('express-handlebars')({ extname: '.hbs ' }))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.listen(3000, () => console.log('http://localhost:3000'))
