b15239b1bd660b793e50b9025425f0bcd077b3fc
ShaneMit <shanemit@cox.net>
2020-05-31 22:30:42 -0700

    added conrtroller to model

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
-const burger = require('../models/burger.js')
+const { Burger } = require('../models/burger.js')
 
-// Create router
+// GET all burgers
 
 module.exports = router
--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,14 @@
-const burger = require('../config/orm')
+const orm = require('../config/orm.js')
 
-// Call ORM functions using buger specific unout for the ORM
+module.exports = {
+  getBurg (cb) {
+    orm.getAll('burgers', burgers => cb(burgers))
+  },
+  addBurg (burger, cb) {
+    orm.createOne('burgers', burger, info => cb(info))
+  },
+  updateBurg (changes, where, cb) {
+    orm.updateOne('burgers', changes, where, info => cb(info))
+  }
+}
 
-module.exports = burger
