95c04650f3524c433b860dcfd39ad486be3c9b58
ShaneMit <shanemit@cox.net>
2020-05-31 20:22:46 -0700

    Built ORM.js

--- config/orm.js
+++ config/orm.js
@@ -1,10 +1,28 @@
-const orm = require('./connection.js')
+const db = require('./connection.js')
 
-// selectAll
+module.exports = {
+  // SELECT all
+  getAll(table,cb) {
+    db.query(`SELECT * FROM ${table}`, (err, data) => {
+      if (err) { console.log(err) }
+      cb(data)
+    })
+  },
+  // INSERT one
+  createOne (table, data, cb) {
+    db.query(`INSERT INTO ${table} SET ?`, data, (err, info) => {
+      if (err) { console.log(err) }
+      cb(info)
+    })
+  },
+  // UPDATE one
+  updateOne (table, changes, where, cb) {
+    db.query(`UPDATE ${table} SET ? WHERE ?`,[changes, where], (err,info) => {
+      if (err) { console.log(err) }
+      cb(info)
+    })
+  }
+}
 
-// insertOne
-
-// updateOne
 
 
-module.exports = orm
