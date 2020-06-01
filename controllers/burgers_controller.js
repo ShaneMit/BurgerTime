03e7ec9dad849fe67f595d2d416e5f33910d786e
ShaneMit <shanemit@cox.net>
2020-05-31 22:37:38 -0700

    built routes in controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,5 +2,15 @@ const router = require('express').Router()
 const { Burger } = require('../models/burger.js')
 
 // GET all burgers
+router.get('/burgers', (req, res) => Burger
+.getBurg(burgers => res.json(burgers)))
+
+// POST one burger
+router.post('/burgers', (req, res) => Burger
+.addBurg(req.body, info => res.json(info)))
+
+// PUT one burger
+router.put('/burgers/:id', (req, res) => Burger
+.updateBurger(req.body, { id: req.params.id }, info => res.json(info)))
 
 module.exports = router
